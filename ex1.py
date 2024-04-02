83e341f3495a3e393ae4fc8fa4c8f7743d45c5ab
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-04-02 09:45:25 +0000

    shape and 3d array layers

--- ex1.py
+++ ex1.py
@@ -3,8 +3,17 @@ import numpy as np
 #creating one,two and three dim array
 a=np.array([5,7],dtype=np.float64)
 b=np.array([[1,2],[3,4],[5,6],[4,7]])
-c=np.array([[[3,4],[5,7],[7,9]]])
+#1 layer
+d=np.array([[[3,4],[5,7],[7,9]]])
+#two layers
+c=np.array([[[3,4],[5,7],[7,9]],[[5,3],[4,7],[3,3]]])
+#finding dim of array
 print(a.ndim,b.ndim,c.ndim)
 #type of array
-print(a.dtype)
-print(b.dtype)
+print(a.dtype,b.dtype)
+#size of item
+print(a.itemsize,b.itemsize)
+#total no of elem in array
+print(a.size,b.size,c.size)
+
+print(a.shape,b.shape,c.shape,d.shape)
